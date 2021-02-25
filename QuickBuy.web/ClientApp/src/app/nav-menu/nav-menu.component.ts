2a9208fd742ca4c4770d3d20cb287d528da1b6a9
Wendell Santos <wendellcrifort@gmail.com>
2021-02-25 11:32:32 -0300

    Criação da estutura básica de autenticação

--- QuickBuy.web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ QuickBuy.web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -8,11 +9,24 @@ import { Component } from '@angular/core';
 export class NavMenuComponent {
   isExpanded = false;
 
+  constructor(private router: Router) {
+
+  }
+
   collapse() {
     this.isExpanded = false;
   }
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  usuarioLogado(): boolean {
+    return sessionStorage.getItem("usuario-autenticado") == "1"
+  }
+
+  sair() {
+    sessionStorage.setItem("usuario-autenticado", "0");
+    this.router.navigate(['/']);
   }
 }
--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core"
+import { Usuario } from "../../model/usuario"
+import { ActivatedRoute, Router } from "@angular/router"
 
 @Component({
   selector: "app-login",
@@ -7,5 +9,32 @@ import { Component } from "@angular/core"
 })
 
 export class LoginComponent {
+  public usuario;  
+  public returnUrl: string
 
+  constructor(private router: Router, private actvatedRouter: ActivatedRoute) {
+    this.usuario = new Usuario();
+    this.returnUrl = this.actvatedRouter.snapshot.queryParams['returnUrl']
+
+  }
+
+  entrar() {
+    if (this.usuario.email == "wendell@teste.com" && this.usuario.senha == "123") {
+      sessionStorage.setItem("usuario-autenticado", "1");      
+      if (this.returnUrl === undefined) {        
+        this.router.navigate(['/']) 
+      }
+      else {
+        this.router.navigate([this.returnUrl])
+      }     
+    }
+  }
+
+  usuarioLogado() {
+
+  }
+
+  sair() {
+
+  }
 }
